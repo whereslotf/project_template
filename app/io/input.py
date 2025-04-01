26e845b9fbba8e6f3f7bc5590e5859d04b2b0cc5
Anastasiia Shevchuk <whereslotf@gmail.com>
2025-04-01 20:52:08 +0300

    input & output functions added

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def input_from_console():
+    pass
+
+def read_file_builtin(file_path):
+    pass
+
+def read_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def print_to_console(text):
+    pass
+
+def write_to_file(file_path, text):
+    pass
+
+def output_to_console_and_file(text, file_path):
+    pass
